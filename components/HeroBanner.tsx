11720a93413296a9819a991719d7cb46d023c7c3
Amine Oussi <63927952+AmineOussi@users.noreply.github.com>
2026-04-26 12:17:55 +0100

    Update HeroBanner.tsx

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -101,8 +101,8 @@ export default function HeroBanner({ movies }: Props) {
             <Image
               src={getImageUrl(movie.poster_path, "w300")}
               alt={title}
-              width={300}
-              height={450}
+              width={128}
+              height={192}
               className="w-full h-auto block"
             />
           </div>
