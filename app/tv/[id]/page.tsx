11131855a956f37bf8316e4bdb78ad4727c02414
anas lasri <anaslasricsgo@gmail.com>
2026-06-25 19:23:07 +0100

    "layout change in movie/tv pages"

--- app/tv/[id]/page.tsx
+++ app/tv/[id]/page.tsx
@@ -83,7 +83,7 @@ async function ShowContent({ id }: { id: number }) {
       </div>
 
       {/* Content */}
-      <div className="px-4 md:px-12 -mt-32 md:-mt-48 relative z-10">
+      <div className="px-4 md:px-12 -mt-32 md:-mt-120 relative z-10">
         <div className="flex flex-col md:flex-row gap-6 md:gap-10 max-w-6xl">
           {/* Poster */}
           <div className="shrink-0 w-52 md:w-72 lg:w-96 self-start rounded-2xl overflow-hidden shadow-2xl ring-1 ring-white/10">
